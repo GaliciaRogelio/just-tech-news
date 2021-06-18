b88025d9c22d1160a2209fa3b8d602cefdf1557a
Rogelio <rogeliogalicia@Rogelios-Air.attlocal.net>
2021-06-17 21:06:14 -0700

    add helper functions to handlebars.js

---
+++ __tests__/helpers.test.js
@@ -0,0 +1,25 @@
+const { format_date, format_plural, format_url } = require("../utils/helpers");
+
+test("format_date() returns a date string", () => {
+  const date = new Date("2021-6-17 19:31:05");
+
+  expect(format_date(date)).toBe("6/17/2021");
+});
+
+test("format_plural() returns a pluralized word", () => {
+  const word = format_plural("tiger");
+  //   const amount = (2, 1);
+
+  expect(word).toBe("tigers");
+  //   expect(format_plural(amount)).toBe("2", "1");
+});
+
+test("format_url() returns a simplified url string", () => {
+  const url1 = format_url("http://test.com/page/1");
+  const url2 = format_url("https://www.coolstuff.com/abcdefg/");
+  const url3 = format_url("https://www.google.com?q=hello");
+
+  expect(url1).toBe("test.com");
+  expect(url2).toBe("coolstuff.com");
+  expect(url3).toBe("google.com");
+});
