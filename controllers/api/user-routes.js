bf4b9e7cb855548c0aa3405f44699008cf2fd30b
Rogelio <rogeliogalicia@Rogelios-Air.attlocal.net>
2021-06-18 17:58:53 -0700

    create an edit post page, finished the dashboard for users only

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,6 +1,5 @@
 const router = require("express").Router();
 const { User, Post, Vote, Comment } = require("../../models");
-const withAuth = require("../../utils/auth");
 
 // GET /api/users
 router.get("/", (req, res) => {
@@ -58,7 +57,7 @@ router.get("/:id", (req, res) => {
 });
 
 // POST /api/users
-router.post("/", withAuth, (req, res) => {
+router.post("/", (req, res) => {
   // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
   User.create({
     username: req.body.username,
@@ -75,7 +74,7 @@ router.post("/", withAuth, (req, res) => {
   });
 });
 
-router.post("/login", withAuth, (req, res) => {
+router.post("/login", (req, res) => {
   // expects {email: 'lernantino@gmail.com', password: 'password1234'}
   User.findOne({
     where: {
@@ -106,7 +105,7 @@ router.post("/login", withAuth, (req, res) => {
 });
 
 // PUT /api/users/1
-router.put("/:id", withAuth, (req, res) => {
+router.put("/:id", (req, res) => {
   // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
 
   // if req.body has exact key/value pairs to match the model, you can just use 'req.body' instead
@@ -130,7 +129,7 @@ router.put("/:id", withAuth, (req, res) => {
 });
 
 // DELETE /api/users/1
-router.delete("/:id", withAuth, (req, res) => {
+router.delete("/:id", (req, res) => {
   User.destroy({
     where: {
       id: req.params.id,
@@ -149,7 +148,7 @@ router.delete("/:id", withAuth, (req, res) => {
     });
 });
 
-router.post("/logout", withAuth, (req, res) => {
+router.post("/logout", (req, res) => {
   if (req.session.loggedIn) {
     req.session.destroy(() => {
       res.status(204).end();
